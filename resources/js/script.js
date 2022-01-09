25c4eea6626aae410f6cc15d4b68901f81b7d398
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2022-01-09 19:43:36 +0630

    team section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,24 @@
+// Preloader
 $(window).on('load', function() {
     $('#status').fadeOut(1000);
     $('#preloader').delay(1100).fadeOut(1000);
-});  
+});
+
+// Carousel Plugin
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel(
+        {
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                1000:{
+                    items:2
+                }
+            }
+        }
+    );
+})
